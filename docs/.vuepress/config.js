81d151eb1fe303eff60876312b61cffe115aedb2
wenchaoyue <584330116@qq.com>
2020-12-02 11:01:05 +0800

    Y Blog Initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-02 10:20:27
+ * @LastEditTime: 2020-12-02 10:39:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Yblog\docs\.vuepress\config.js
+ */
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around'
+}
---
+++ docs/README.md
@@ -0,0 +1,10 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-02 10:16:27
+ * @LastEditTime: 2020-12-02 10:39:30
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \Yblog\docs\README.md
+-->
+# Hello My Yblog
+
